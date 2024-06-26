a513637be64d5edbdc1dd8bc6f1d1d162b6a6290
Thonas Abegglen <tom@abegglen.be>
2024-06-26 13:47:23 +0200

    clear_table OUT_OUT und IN_IN optimiert

--- Controller.py
+++ Controller.py
@@ -245,10 +245,10 @@ class Controller():
             coors = np.array([[0, self.get_current_rho_position()], [0, 1], [314, 0]])
         elif clear_mode == CLEAR_MODE_OUT_OUT:
             print("CLEAR_MODE_OUT_OUT")
-            coors = np.array([[0, self.get_current_rho_position()], [0, 1], [157, 0], [314, 1]])
+            coors = np.array([[0, self.get_current_rho_position()], [0, 1], [157, 0], [160.14, 0], [314, 1]])
         elif clear_mode == CLEAR_MODE_IN_IN:
             print("CLEAR_MODE_IN_IN")
-            coors = np.array([[0, self.get_current_rho_position()], [0, 0.1], [157, 1], [314, 0]])
+            coors = np.array([[0, self.get_current_rho_position()], [0, 0.1], [157, 1], [160.14, 1], [314, 0]])
         else:
             print("default: CLEAR_MODE_IN_OUT")
             coors = np.array([[0, self.get_current_rho_position()], [0, 0.1], [314, 1]])
